fb60ee82a098cf319d22361bdc4d59a1b2228e25
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-01-17 18:34:59 +0530

    Add files via upload

---
+++ QuestionModel.kt
@@ -0,0 +1,11 @@
+package com.example.quizzify.Class
+
+class QuestionModel(var Question:String,var option1:String,var option2:String,var option3:String,var option4:String, var ans:String ) {
+//    var Question:String?=null
+//    var option1:String?=null
+//    var option2:String?=null
+//    var option3:String?=null
+//    var option4:String?=null
+//    var ans:String?=null
+
+}
